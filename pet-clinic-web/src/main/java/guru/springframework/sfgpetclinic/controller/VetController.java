e01a897ffb666cd2f9518909b7d7b6b4742f2224
mimoh <mimoh@DESKTOP-I23K3L6>
2020-04-13 22:50:26 +0200

    Added Owners page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listsVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listsVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
